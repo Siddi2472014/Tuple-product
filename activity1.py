75532a0681ebd5a282d31dfa85c31445e4041023
Siddi2472014 <jagadeeswarisiddi85@gmail.com>
2026-03-17 15:25:55 +0800

    Refactor and add print statements for tuples

--- activity1.py
+++ activity1.py
@@ -3,17 +3,15 @@ def calculate_product(numbers):
     for num in numbers:
         product *= num
     return product
-numbers_tuple = (4,3,2,2,-1,18)
-print(numbers_tuple)
-result = calculate_product(numbers_tuple)
-print("The product of the numbers in the tuple is:", result)
 
-def calculate_product(numbers):
-    product = 1
-    for num in numbers:
-        product *= num
-    return product
-numbers_tuple2 = (2,4,8,8,3,2,9)
-print(numbers_tuple)
-result = calculate_product(numbers_tuple)
-print("The product of the numbers in the tuple is:", result)
+# First tuple
+numbers_tuple1 = (4, 3, 2, 2, -1, 18)
+print(numbers_tuple1)
+result1 = calculate_product(numbers_tuple1)
+print("The product of the numbers in the tuple is:", result1)
+
+# Second tuple
+numbers_tuple2 = (2, 4, 8, 8, 3, 2, 9)
+print(numbers_tuple2)
+result2 = calculate_product(numbers_tuple2)
+print("The product of the numbers in the tuple is:", result2)
